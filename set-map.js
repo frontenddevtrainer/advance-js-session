fc8f5ee18efd638af8837561d231523740b82d37
Admin <admin@example.com>
2024-03-11 10:57:36 +0000

    set and map

---
+++ set-map.js
@@ -0,0 +1,13 @@
+// const person = new Map();
+// person.set("name", "Praveen")
+
+// const people = new Set();
+
+// people.add("Praveen")
+// people.add("John")
+// people.add("John")
+// people.add("John")
+// people.add("John")
+// people.add("Praveen")
+
+// console.log(people);

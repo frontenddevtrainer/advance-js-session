fcebe12a47532143d395e79d18832c894976cc07
Admin <admin@example.com>
2024-03-12 10:46:44 +0000

    classes and interface.

--- application.ts
+++ application.ts
@@ -1,2 +1,39 @@
-const message: string = "Hello World!!!";
-console.log(message);
+interface IPerson {
+  name: string;
+  age: number;
+  city?: string | null;
+  sayHello(name: string) : string
+}
+
+interface IManager {
+    isManager: boolean
+}
+
+class Person implements IPerson, IManager {
+  sayHello(name: string): string {
+      return `Hi ${name}`
+  }
+  age = 23;
+  name = "Praveen";
+  city = "DEL";
+  isManager = false;
+}
+
+// const person: Person = {
+//   name: "praveen",
+//   age: 32,
+// };
+
+// const people: Person[] = [person]
+
+// people[0].city
+
+// const message: string = "hello world";
+// const age: number = 54;
+// const isAdminUser: boolean = false;
+// const isSuperUser: boolean = true;
+// const userCity: string | null = "DEL";
+
+// function userCountry(countryCode: string): Promise<string> {
+//   return Promise.resolve(countryCode.toLowerCase());
+// }
